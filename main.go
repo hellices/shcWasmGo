191b02dd7a029d77753a881f5fd8e3c3774574dc
MH YOO <mhyoo@MHui-MacBookAir.local>
2023-06-10 12:07:14 +0900

    poc_test

--- main.go
+++ main.go
@@ -1,32 +1,60 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strings"
 
 	spinhttp "github.com/fermyon/spin/sdk/go/http"
-	"github.com/wasm_poc/index"
 )
 
 func init() {
 
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
 
-		if strings.Contains(r.URL.RequestURI(), ".js") {
+		url := r.URL.RequestURI()
+		var data []byte
+
+		switch {
+		case strings.Contains(url, "dashboard.js"):
 			w.Header().Set("Content-Type", "application/javascript")
-			data := index.GetDashboard()
+			// get from raw.githubusercontent
+			resp, _ := spinhttp.Get("https://raw.githubusercontent.com/hellices/shcWasmGo/poc/static/dashboard.js")
+			data, _ = ioutil.ReadAll(resp.Body)
 			w.Write([]byte(data))
-		} else {
-			if strings.Contains(r.URL.RequestURI(), "poc") {
-				w.Header().Set("Content-Type", "text/html")
-				data := index.GetWasm()
-				w.Write([]byte(data))
-			} else {
-				w.Header().Set("Content-Type", "text/html")
-				data := index.GetIndex()
-				w.Write([]byte(data))
-			}
+		case strings.Contains(url, "/demo/spin"):
+			w.Header().Set("Content-Type", "text/html")
+			resp, _ := spinhttp.Get("https://raw.githubusercontent.com/hellices/shcWasmGo/poc/static/demo_spin.html")
+			data, _ = ioutil.ReadAll(resp.Body)
+			w.Write([]byte(data))
+		case strings.Contains(url, "/"):
+			w.Header().Set("Content-Type", "text/html")
+			resp, _ := spinhttp.Get("https://raw.githubusercontent.com/hellices/shcWasmGo/poc/static/index.html")
+			data, _ = ioutil.ReadAll(resp.Body)
+			w.Write([]byte(data))
+		default:
+			w.WriteHeader(404)
+			w.Header().Set("Content-Type", "text/html")
+			resp, _ := spinhttp.Get("https://raw.githubusercontent.com/hellices/shcWasmGo/poc/static/404.html")
+			data, _ = ioutil.ReadAll(resp.Body)
+			w.Write([]byte(data))
 		}
+
+		// if strings.Contains(r.URL.RequestURI(), ".js") {
+		// 	w.Header().Set("Content-Type", "application/javascript")
+		// 	data := index.GetDashboard()
+		// 	w.Write([]byte(data))
+		// } else {
+		// 	if strings.Contains(r.URL.RequestURI(), "poc") {
+		// 		w.Header().Set("Content-Type", "text/html")
+		// 		data := index.GetWasm()
+		// 		w.Write([]byte(data))
+		// 	} else {
+		// 		w.Header().Set("Content-Type", "text/html")
+		// 		data := index.GetIndex()
+		// 		w.Write([]byte(data))
+		// 	}
+		// }
 
 	})
 }
