729269d695428d2417c3b4fe04020a47b0723b33
MH YOO <mhyoo@MHui-MacBookAir.local>
2023-06-10 12:20:40 +0900

    merge

--- main.go
+++ main.go
@@ -39,23 +39,6 @@ func init() {
 			data, _ = ioutil.ReadAll(resp.Body)
 			w.Write([]byte(data))
 		}
-
-		// if strings.Contains(r.URL.RequestURI(), ".js") {
-		// 	w.Header().Set("Content-Type", "application/javascript")
-		// 	data := index.GetDashboard()
-		// 	w.Write([]byte(data))
-		// } else {
-		// 	if strings.Contains(r.URL.RequestURI(), "poc") {
-		// 		w.Header().Set("Content-Type", "text/html")
-		// 		data := index.GetWasm()
-		// 		w.Write([]byte(data))
-		// 	} else {
-		// 		w.Header().Set("Content-Type", "text/html")
-		// 		data := index.GetIndex()
-		// 		w.Write([]byte(data))
-		// 	}
-		// }
-
 	})
 }
 
