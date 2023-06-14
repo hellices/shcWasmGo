c5e07fd62681389e72f34b2beb66775d3b0a7f73
MHYOO <mhyoo@MHYOOui-Macmini.local>
2023-06-14 22:10:24 +0900

    poc

--- test/host/http/http.go
+++ test/host/http/http.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,27 +8,6 @@ import (
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte("Hello World"))
-	})
-
-	http.HandleFunc("/robots", func(w http.ResponseWriter, req *http.Request) {
-		// GET 호출
-		resp, err := http.Get("https://google.com/robots.txt")
-
-		if err != nil {
-			panic(err)
-		}
-
-		defer resp.Body.Close()
-
-		// 결과 출력
-		data, err := ioutil.ReadAll(resp.Body)
-
-		if err != nil {
-			panic(err)
-		}
-
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write([]byte(data))
 	})
 
 	log.Panic(http.ListenAndServe(":50001", nil))
